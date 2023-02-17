251cb42fd4b10ed4c736ee2d00b697489adbb6de
Søren Bruus Zeppelin <sz@concordium.com>
2023-02-17 09:20:28 +0100

    Renamed concordium network specifier (genesis hash) env variable to be generic

--- frontend/.env.d.ts
+++ frontend/.env.d.ts
@@ -2,21 +2,16 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             // Ethereum contract addresses
-            NEXT_PUBLIC_WETH: string; // TODO: Doesn't seem like this is used? Can this be removed?
+            // NEXT_PUBLIC_WETH: string; // TODO: Doesn't seem like this is used? Can this be removed?
             /**
              * Address of the bridge root manager contract on Ethereum.
              */
             NEXT_PUBLIC_ROOT_MANAGER_ADDRESS: string;
             NEXT_PUBLIC_GENERATE_ETHER_PREDICATE_ADDRESS: string; // TODO: What's this? Add documentation
-            NEXT_PUBLIC_GENERATE_ERC20_PREDICATE_ADDRESS: string; // TODO: Doesn't seem like this is used? Can this be removed?
-            NEXT_PUBLIC_STATE_SENDER_ADDRESS: string; // TODO: Doesn't seem like this is used? Can this be removed?
-            NEXT_PUBLIC_PROXY_ADMIN_ADDRESS: string; // TODO: Doesn't seem like this is used? Can this be removed?
+            // NEXT_PUBLIC_GENERATE_ERC20_PREDICATE_ADDRESS: string; // TODO: Doesn't seem like this is used? Can this be removed?
+            // NEXT_PUBLIC_STATE_SENDER_ADDRESS: string; // TODO: Doesn't seem like this is used? Can this be removed?
+            // NEXT_PUBLIC_PROXY_ADMIN_ADDRESS: string; // TODO: Doesn't seem like this is used? Can this be removed?
 
-            // Concordium contract addresses
-            /**
-             * Bridge manager contract index. For now, the accompanying subindex is assumed to be 0. 
-             */
-            NEXT_PUBLIC_BRIDGE_MANAGER_INDEX: number; // TODO: Don't assume the subindex is 0, as sometime it might not be...
 
             // Ethereum token contract addresses
             /**
@@ -29,15 +24,24 @@ declare global {
             NEXT_PUBLIC_WETH_TOKEN_ADDRESS: string;
 
             // Providers
+            /**
+             * The ethereum network ID
+             */
             NEXT_PUBLIC_ETHEREUM_PROVIDER_NETWORK: string; // TODO: What's this? Document...
-            NEXT_PUBLIC_ETHEREUM_PROVIDER_KEY: string; // TODO: What's this? Document...
-            NEXT_PUBLIC_INFURA_PROJECT_ID: string; // TODO: What's this? Document...
+            // NEXT_PUBLIC_ETHEREUM_PROVIDER_KEY: string; // TODO: What's this? Document...
+            // NEXT_PUBLIC_INFURA_PROJECT_ID: string; // TODO: What's this? Document...
 
             /**
-             * Block hash of testnet genesis block.
-             * Is used to check that the user has its browser wallet connected to testnet and not to mainnet.
+             * Block hash of genesis block of the network.
+             * Is used to check that the user has its browser wallet connected to the correct network.
              */
-            NEXT_PUBLIC_TESTNET_GENESIS_BLOCK_HASH: string; // TODO: Remove this and hardcode instead.
+            NEXT_PUBLIC_NETWORK_GENESIS_BLOCK_HASH: string; // TODO: Remove this and hardcode instead.
+
+            // Concordium contract addresses
+            /**
+             * Bridge manager contract index. For now, the accompanying subindex is assumed to be 0. 
+             */
+            NEXT_PUBLIC_BRIDGE_MANAGER_INDEX: number; // TODO: Don't assume the subindex is 0, as sometime it might not be...
 
             /**
              * URL of the bridge API.
