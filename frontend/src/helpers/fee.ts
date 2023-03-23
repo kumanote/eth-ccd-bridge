bf5385745aef126861917e8e42c571272496820b
Søren Bruus Zeppelin <soerenbf@gmail.com>
2023-03-23 14:50:21 +0100

    Remove or lax rounding where applicable

--- frontend/src/helpers/fee.ts
+++ frontend/src/helpers/fee.ts
@@ -5,9 +5,9 @@ const CCD_DECIMALS = 6;
 const microCcdToCcd = (number: BigNumberish) => ethers.utils.formatUnits(number, CCD_DECIMALS);
 
 export const renderGasFeeEstimate = (fee: number, ethPrice: number): string =>
-    `~${fee.toFixed(4)} ETH (${(fee * ethPrice).toFixed(4)} USD)`;
+    `~${fee.toFixed(8)} ETH (~${(fee * ethPrice).toFixed(4)} USD)`;
 
 export const renderEnergyFeeEstimate = (microCcdFee: bigint, ccdPrice: number): string => {
     const ccdFee = Number(microCcdToCcd(microCcdFee));
-    return `~${ccdFee.toFixed(4)} CCD (${(ccdFee * ccdPrice).toFixed(4)} USD)`;
+    return `~${ccdFee.toFixed(4)} CCD (~${(ccdFee * ccdPrice).toFixed(4)} USD)`;
 };
