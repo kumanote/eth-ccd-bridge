c4819368bf08290fc4ab2b66676c850cfba09e29
Søren Bruus Zeppelin <soerenbf@gmail.com>
2023-03-29 10:26:17 +0200

    Improve api query configuration to lessen the amount of network requests

--- frontend/src/api-query/queries.ts
+++ frontend/src/api-query/queries.ts
@@ -7,13 +7,13 @@ import isDeposit from "src/helpers/checkTransaction";
 import { isDefined } from "src/helpers/basic";
 
 /**
- * Interval in ms for querying merkle proof
- */
-const MERKLE_UPDATE_INTERVAL = 60000;
-/**
  * Interval in ms for querying in general
  */
-const QUERY_UPDATE_INTERVAL = 10000;
+const QUERY_UPDATE_INTERVAL = 60000;
+/**
+ * Interval in ms for querying in individual transaction status
+ */
+const WATCH_TRANSACTION_INTERVAL = 15000;
 
 type WatchWithdrawParams = Paths.WatchWithdrawTx.PathParameters;
 type WatchWithdrawOptions = UseQueryOptions<
@@ -37,12 +37,12 @@ export const useWatchWithdraw = (params?: WatchWithdrawParams, options?: WatchWi
         {
             ...options,
             refetchInterval: (data, query) => {
-                if (data?.concordium_event_id !== undefined) {
+                if (data?.concordium_event_id !== null && data?.concordium_event_id !== undefined) {
                     return false;
                 }
 
                 if (options?.refetchInterval === undefined) {
-                    return QUERY_UPDATE_INTERVAL;
+                    return WATCH_TRANSACTION_INTERVAL;
                 }
 
                 return typeof options?.refetchInterval === "function"
@@ -75,12 +75,12 @@ export const useWatchDeposit = (params?: WatchDepositParams, options?: WatchDepo
         {
             ...options,
             refetchInterval: (data, query) => {
-                if (data?.concordium_tx_hash !== undefined) {
+                if (data?.concordium_tx_hash) {
                     return false;
                 }
 
                 if (options?.refetchInterval === undefined) {
-                    return QUERY_UPDATE_INTERVAL;
+                    return WATCH_TRANSACTION_INTERVAL;
                 }
 
                 return typeof options?.refetchInterval === "function"
@@ -91,7 +91,7 @@ export const useWatchDeposit = (params?: WatchDepositParams, options?: WatchDepo
     );
 };
 
-export const useWalletTransactions = () => {
+export const useWalletTransactions = (refetch = false) => {
     const { context } = useEthWallet();
     const { getClient } = useAxiosClient();
 
@@ -109,12 +109,12 @@ export const useWalletTransactions = () => {
             const { data } = await client.wallet_txs({ wallet });
             return data;
         },
-        { refetchInterval: QUERY_UPDATE_INTERVAL }
+        { refetchInterval: refetch && QUERY_UPDATE_INTERVAL, enabled: refetch }
     );
 };
 
 export const usePendingWithdrawals = () => {
-    const result = useWalletTransactions();
+    const result = useWalletTransactions(true);
 
     const data = result.data
         ?.map((tx) => {
@@ -140,7 +140,7 @@ export const useTokens = () => {
             const { data } = await client.list_tokens();
             return data;
         },
-        { refetchOnWindowFocus: false }
+        { staleTime: Infinity }
     );
 };
 
@@ -165,10 +165,10 @@ export const useEthMerkleProof = (params: Partial<MerkleProofParams>, enabled = 
             enabled:
                 params.tx_hash !== undefined && params.event_id !== undefined && params.event_id !== null && enabled,
             refetchInterval: (data) => {
-                if (data?.proof !== undefined) {
+                if (data?.proof) {
                     return false;
                 }
-                return MERKLE_UPDATE_INTERVAL;
+                return QUERY_UPDATE_INTERVAL;
             },
         }
     );
@@ -187,7 +187,7 @@ export const useNextMerkleRoot = () => {
             return data;
         },
         {
-            refetchInterval: MERKLE_UPDATE_INTERVAL,
+            refetchInterval: QUERY_UPDATE_INTERVAL,
         }
     );
 };
